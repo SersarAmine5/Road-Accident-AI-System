a6c5d1c633f053b444a9260ddac84e2c749dd684
darkpaladium <95679935+darkpaladium@users.noreply.github.com>
2026-03-28 15:50:01 +0100

    ajout de params

--- src/train.py
+++ src/train.py
@@ -1,6 +1,15 @@
 from ultralytics import YOLO
+import argparse
 
-model = YOLO("yolov8n.pt")
+parser = argparse.ArgumentParser(description="Simple file processor")
+
+parser.add_argument("--epochs", default=10,type=int)
+parser.add_argument("--data", default="./data/data.yaml" )
+parser.add_argument("--model", default="./models/yolov8n.pt")
+
+args = parser.parse_args()
+
+model = YOLO(args.model)
 
 # TODO: faire en sorte que les parameters d'entrainement soit modifiables.
 # python src/train.py epochs=20
@@ -9,7 +18,7 @@ model = YOLO("yolov8n.pt")
 # entrainement
 # evaluation
 results = model.train(
-    data="./data/data.yaml",
-    epochs=10,
+    data=args.data,
+    epochs=args.epochs,
     imgsz=640,
 )
